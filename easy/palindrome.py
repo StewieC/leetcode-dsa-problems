e83a07d94e5eb5a2a21544ec0166544379cb9bc4
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-09-23 18:52:45 +0300

    add isPalindrome function to check if an integer is a palindrome

---
+++ easy/palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, x:int) -> bool:
+        # handle negative numbers
+        if x<0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        # handle single digits
+        if x<10:
+            return True
+        
+        reversed_num = 0
+        while x > reversed_num:
+            digit = x % 10
+            reversed_num = reversed_num * 10 + digit
+            x = x // 10
+            
+        return x == reversed_num or x == reversed_num // 10
+        
